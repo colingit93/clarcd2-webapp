f21f4b152eae8c6a14147cfbecf8915336006ed5
Colin <colin.jochum@edu.fh-joanneum.at>
2021-11-07 12:52:30 +0100

    Readme and v2.0

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 CLARCD2-TestApp
 
 Aufgabe02 (2) Simple Node JS WebApp for Azure
+
+Steps to Deploy 
+
+az login --tenant xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxx
+az deployment group create -f ./webdeployment.bicep -g CLARCD2_Jochum --mode complete
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("CLARCD2 Colin Jochum FH-JOANNEUM Web App");
+    response.end("CLARCD2 Colin Jochum FH-JOANNEUM Web App v2.0");
 });
 
 const port = process.env.PORT || 1337;
