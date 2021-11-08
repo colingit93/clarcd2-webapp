4e5c19e9f321b0a3f7a7cb3b9498971f3f7d9eb3
Colin <colin.jochum@edu.fh-joanneum.at>
2021-11-08 12:24:30 +0100

    New Version 0.004 Deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("CLARCD2 Colin Jochum FH-JOANNEUM Web App v0.003");
+    response.end("CLARCD2 Colin Jochum FH-JOANNEUM Web App v0.004");
 });
 
 const port = process.env.PORT || 1337;
