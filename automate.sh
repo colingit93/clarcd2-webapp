569fbcea1c72f5f35406a3314dc853024bd3de64
Colin <40957191+colingit93@users.noreply.github.com>
2021-11-15 11:22:35 +0100

    Delete automate.sh

--- automate.sh
+++
@@ -1,3 +0,0 @@
-az login --tenant db9bb29f-b9ec-4a37-81d4-5cf38b748984
-az resource delete --resource-group CLARCD2_Jochum --name jochumclarcd-pe3rjptmp52s4 --resource-type "Microsoft.Web/sites"
-az deployment group create -f ./webdeployment.bicep -g CLARCD2_Jochum
